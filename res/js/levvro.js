ef1fb38965ebaf5b4fd9c58c787efd43fc1def36
Vimal Atreya Ramaka <vimmy2492@gmail.com>
2013-11-27 13:47:11 -0400

    type ahead

--- res/js/levvro.js
+++ res/js/levvro.js
@@ -1,18 +1,24 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     $("#edition").select2({
-    	placeholder: "Select a Faculty",
+        placeholder: "Select a Faculty",
     });
 
     $("#condition").select2({
-    	placeholder: "Select a Faculty",
+        placeholder: "Select a Faculty",
     });
 
     $("#select-faculty").select2({
-    	placeholder: "Select a Faculty",
+        placeholder: "Select a Faculty",
+    });
+
+    $('.courses-typehead').typeahead({
+        name: 'coursenames',
+        local: ["BUS101 Introduction to Business","CSC151 Introduction to Programming","BUS251 Introduction to Management Science","POT101 Introduction to Potatoes","EC101 Introduction to Microeconomics"],
+        autoselect: 'first',
     });
 });
 
-$('.listing-collapse').click(function(){
-	$(this).nextUntil('tr.listing-collapse').slideToggle(0);
-	$(this).toggleClass("warning");
+$('.listing-collapse').click(function () {
+    $(this).nextUntil('tr.listing-collapse').slideToggle(0);
+    $(this).toggleClass("warning");
 });
